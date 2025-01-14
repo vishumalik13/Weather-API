0bfbb3ff46faab279c9598c2015eac9c576cd05c
Vishu Malik <vishumalik132004@gmail.com>
2025-01-15 01:33:56 +0530

    Inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import pyttsx3
+
+city=input("Enter the name of the city \n")
+
+url=f"https://api.weatherapi.com/v1/current.json?key=b9357d49fce74a23ac963130251301&q={city}"
+r=requests.get(url)
+# print(r.text)
+wdict=json.loads(r.text)
+w=wdict["current"]["temp_c"]
+engine=pyttsx3.init()
+engine.say(f"the current weather in {city} is {w} degree")
+engine.runAndWait()
